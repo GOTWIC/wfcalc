827791c123379e48b99feb940fba25d5e41918c6
GOTWIC <50720527+GOTWIC@users.noreply.github.com>
2022-01-09 12:34:49 -0500

    Add files via upload

--- functions.js
+++ functions.js
@@ -4,46 +4,52 @@ const slots = ["primary", "secondary", "melee"];
 
 function toggle(slot){
 
+
+
   if(slot == "primary"){
     if(primary == 0){
       document.getElementById(slot).style.display="none";
+      document.getElementById("slotbtn").innerHTML = "Select Slot";
       return primary = 1;
     }
 
     else{
       document.getElementById(slot).style.display="inline";
       singleActive(slots, slot);
-      return primary = 0;
+      document.getElementById("slotbtn").innerHTML = "Primary";
+      return primary = 0, secondary = 1, melee = 1;
     }
   }
 
   if(slot == "secondary"){
     if(secondary == 0){
       document.getElementById(slot).style.display="none";
+      document.getElementById("slotbtn").innerHTML = "Select Slot";
       return secondary = 1;
     }
 
     else{
       document.getElementById(slot).style.display="inline";
       singleActive(slots, slot);
-      return primary = 0;
+      document.getElementById("slotbtn").innerHTML = "Secondary";
+      return secondary = 0, primary = 1, melee = 1;
     }
   }
 
   if(slot == "melee"){
     if(melee == 0){
       document.getElementById(slot).style.display="none";
+      document.getElementById("slotbtn").innerHTML = "Select Slot";
       return melee = 1;
     }
 
     else{
       document.getElementById(slot).style.display="inline";
       singleActive(slots, slot);
-      return melee = 0;
+      document.getElementById("slotbtn").innerHTML = "Melee";
+      return melee = 0, primary = 1, secondary = 1;
     }
   }
-
-
 }
 
 function singleActive(array, activeItem){
@@ -51,4 +57,10 @@ function singleActive(array, activeItem){
   for (const nonActiveItem of temp) {
     document.getElementById(nonActiveItem).style.display="none";
   }
+}
+
+
+
+function changeSlot(){
+  document.getElementById("slotbtn").innerHTML = "Primary";
 }
